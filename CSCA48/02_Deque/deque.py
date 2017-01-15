e6ed7c24158ba38ee6e44a4d483da7b1d5484373
Eric Wang <eric.myspace@hotmail.com>
2017-01-15 11:20:57 -0500

    change attribute name, pass pep8

--- CSCA48/02_Deque/deque.py
+++ CSCA48/02_Deque/deque.py
@@ -1,5 +1,5 @@
 class EmptyQueueError(Exception):
-    '''An error to be raised when someone tries to dequeue from an empty queue'''
+    ''' An error to be raised when dequeued from an empty deque '''
     pass
 
 
@@ -12,37 +12,37 @@ class Deque():
         Initialize a Dequeue.
         '''
         # Representation invariant:
-        # _contents is a list of object.
-        # _contents[:] are the objects in the dequeue.
-        # if i < j, i >=0, j < len(_contents), then
-        #    _contents[i] is to the left of _contents[j] in the deque
-        #    _contents[j] is to the right of _contents[i] in the deque
-        self._contents = []
+        # _container is a list of object.
+        # _container[:] are the objects in the dequeue.
+        # if i < j, i >=0, j < len(_container), then
+        #    _container[i] is to the left of _container[j] in the deque
+        #    _container[j] is to the right of _container[i] in the deque
+        self._container = []
 
     def enqueue_left(self, item):
         ''' (Deque, object) -> NoneType
 
         Add item onto the left side of the deque
         '''
-        self._contents = [item] + self._contents
+        self._container = [item] + self._container
 
     def enqueue_right(self, item):
         ''' (Deque, object) -> NoneType
 
         Add item onto the right side of the deque
         '''
-        self._contents = self._contents + [item]
+        self._container = self._container + [item]
 
     def dequeue_left(self):
         ''' (Deque) -> object
 
         Remove and return the leftmost item in the deque
         '''
-        if(self.is_empty()):
-            raise EmptyQueueError("Can't dequeue from an empty queue")
+        if (self.is_empty()):
+            raise EmptyQueueError("Can't dequeue left from an empty queue")
         else:
-            item = self._contents[0]
-            self._contents = self._contents[1:]
+            item = self._container[0]
+            self._container = self._container[1:]
             return item
 
     def dequeue_right(self):
@@ -50,11 +50,11 @@ class Deque():
 
         Remove and return the rightmost item in the deque
         '''
-        if(self.is_empty()):
-            raise EmptyQueueError("Can't dequeue from an empty queue")
+        if (self.is_empty()):
+            raise EmptyQueueError("Can't dequeue right from an empty queue")
         else:
-            item = self._contents[-1]
-            self._contents = self._contents[:-1]
+            item = self._container[-1]
+            self._container = self._container[:-1]
             return item
 
     def is_empty(self):
@@ -62,9 +62,10 @@ class Deque():
 
         Return True iff this deque is empty
         '''
-        return len(self._contents) == 0
+        return len(self._container) == 0
 
-if(__name__ == "__main__"):
+
+if (__name__ == "__main__"):
     dq = Deque()
     dq.enqueue_left('A')
     dq.enqueue_left('B')
@@ -73,7 +74,6 @@ if(__name__ == "__main__"):
     dq.enqueue_right('E')
     dq.enqueue_right('F')
     dq.enqueue_left('G')
-    while(not dq.is_empty()):
+    while (not dq.is_empty()):
         print(dq.dequeue_right())
         print(dq.dequeue_left())
-
