c56b1561185bfac079f8923e923d7997a40ea2eb
Eric Wang <eric.myspace@hotmail.com>
2017-03-13 10:51:31 -0400

    implement tree algorithms

--- CSCA48/09_Tree_Algorithms/tree_algorithms.py
+++ CSCA48/09_Tree_Algorithms/tree_algorithms.py
@@ -15,18 +15,20 @@ class BTNode(object):
     def __str__(self):
         return self._str_helper("")
 
-    def _str_helper(self, indentation=""):
+    def _str_helper(self, indent=""):
         """(BTNode, str) -> str
         Return a "sideways" representation of the subtree rooted at this node,
         with right subtrees above parents above left subtrees and each node on
         its own line, preceded by as many TAB characters as the node's depth.
         """
         str_rep = ""
+
         if (self.right):
-            str_rep += self.right._str_helper(indentation + "\t") + "\n"
-        str_rep += indentation + str(self.value) + str(self.sibling) + "\n"
+            str_rep += self.right._str_helper(indent + "\t") + "\n"
+        str_rep += indent + str(self.value) + str(self.sibling) + "\n"
         if (self.left):
-            str_rep += self.left._str_helper(indentation + "\t") + "\n"
+            str_rep += self.left._str_helper(indent + "\t") + "\n"
+
         return str_rep
 
     def get_height(self):
@@ -34,14 +36,29 @@ class BTNode(object):
         Return the height of the tree rooted at this node. We define the
         height of a tree with a single node to be 1
         '''
-        pass
+        left_height = 0
+        right_height = 0
+
+        if (self.left):
+            left_height = self.left.get_height()
+        if (self.right):
+            right_height = self.right.get_height()
+
+        return max(left_height, right_height) + 1  # add root node itself
 
     def set_sibling(self, have_sib=False):
         '''(BTNode[, bool]) -> NoneType
         For each node in the tree rooted at this node, set their
         .sibling variable to True iff they have a sibling
         '''
-        pass
+        self.sibling = have_sib
+        if (self.left and self.right):
+            self.left.set_sibling(True)
+            self.right.set_sibling(True)
+        elif (self.left):
+            self.left.set_sibling(False)
+        elif (self.right):
+            self.right.set_sibling(False)
 
 
 if (__name__ == "__main__"):
