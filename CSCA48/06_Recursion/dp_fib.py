9bdb23f2c899e3232549b5d9d0d54f54ed22fe35
Eric Wang (Mac) <meowster.eric@hotmail.com>
2017-09-21 10:56:14 -0400

    add dp fib

---
+++ CSCA48/06_Recursion/dp_fib.py
@@ -0,0 +1,13 @@
+memo = {}
+def fib(n):
+    if n in memo:
+        return memo[n]
+    if n <= 2:
+        f = 1
+    else:
+        f = fib(n-1) + fib(n-2)
+    memo[n] = f
+    return f
+
+print(fib(10))
+print(memo)
